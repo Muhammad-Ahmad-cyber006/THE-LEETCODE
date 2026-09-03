cd31d3e5e7ace345ef8a571578f90c357f9b8598
Muhammad Ahmad <novahat2580@gmail.com>
2026-09-04 00:52:40 +0500

    Time: 8 ms (38.54%) | Memory: 165.8 MB (84.39%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) 
     {
-        int minOdd=INT_MAX;
+        int minOdd = INT_MAX, minEven = INT_MAX;
 
-        // Sabse chota odd number find karo
-        for(int x:nums1)
+        for(int x : nums1)
         {
-            if(x%2!=0)
-                minOdd = min(minOdd,x);
+            if(x & 1)
+                minOdd = min(minOdd, x);
+            else
+                minEven = min(minEven, x);
         }
 
-        // Agar koi odd nahi hai,already sab even hain
-        if(minOdd==INT_MAX)
+        // Sirf even numbers hain  already uniform
+        if(minOdd == INT_MAX)
             return true;
 
-        for(int x:nums1)
-        {
-            // Even number ko odd banane ke liye
-            // usse chota odd number chahiye
-            if(x%2==0 && x<minOdd)
-                return false;
-        }
-
-        // Ab har even ko smallest odd se subtract karke
-        // odd bana sakte hain, aur odd numbers ko same rakh sakte hain
-        return true;
+        // Sabse chota even, smallest odd se chota hua
+        // toh dono parity ko uniform nahi bana sakte
+        return minEven > minOdd;
     }
 };
