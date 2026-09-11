8d28eb035c8af16a986dd60e7b693b0a70f4ca38
Muhammad Ahmad <novahat2580@gmail.com>
2026-09-11 16:41:53 +0500

    Time: 0 ms (100.00%) | Memory: 28.8 MB (88.78%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -2,20 +2,59 @@ class Solution {
 public:
     int totalNumbers(vector<int>& n) 
     {
-        set<int> s;
-        for(int i=0;i<n.size();i++)
+        int m[10]={0};
+        int count=0;
+
+        // har digit ki frequency store karo
+        for(int x:n)
+            m[x]++;
+
+        // first digit 1-9 hona chahiye 0 nahi
+        for(int i=1;i<10;i++)
         {
-            for(int j=0;j<n.size();j++)
+            if(!m[i]) continue;
+            else m[i]--; // i digit use kar lo
+
+            // second digit 0-9 kuch bhi ho sakta hai
+            for(int j=0;j<10;j++)
             {
-                for(int k=0;k<n.size();k++)
+                if(!m[j]) continue;
+                else m[j]--; // j digit use kar lo
+
+                // last digit even hona chahiye: 0,2,4,6,8
+                for(int k=0;k<10;k+=2)
                 {
-                    if(i==j||i==k||j==k||n[i]==0||n[k]%2!=0)
-                        continue;
-                    int num=n[i]*100+n[j]*10+n[k];
-                    s.insert(num);
+                    if(m[k])
+                        count++;
                 }
+
+                m[j]++; // j digit wapas
             }
+
+            m[i]++; // i digit wapas
         }
-        return s.size();
+
+        return count;
     }
 };
+// class Solution {
+// public:
+//     int totalNumbers(vector<int>& n) 
+//     {
+//         set<int> s;
+//         for(int i=0;i<n.size();i++)
+//         {
+//             for(int j=0;j<n.size();j++)
+//             {
+//                 for(int k=0;k<n.size();k++)
+//                 {
+//                     if(i==j||i==k||j==k||n[i]==0||n[k]%2!=0)
+//                         continue;
+//                     int num=n[i]*100+n[j]*10+n[k];
+//                     s.insert(num);
+//                 }
+//             }
+//         }
+//         return s.size();
+//     }
+// };
